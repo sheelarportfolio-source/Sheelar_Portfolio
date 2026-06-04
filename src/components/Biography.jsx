d3840f3eb71872ce1cb99806cd391aee2267980a
Sheelar <sheelarportfolio@gmail.com>
2026-06-04 15:30:22 +0530

    Updated portfolio website

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -25,7 +25,7 @@ const Biography = ({ setActiveTab }) => {
             </a>
             <div className="connect-wrapper">
               <span className="connect-text">CONNECT</span>
-              <a href="https://www.linkedin.com/in/sheela-r-592b2722/" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <a href="https://www.linkedin.com/in/dr-r-sheela-chandrashekar-592b2722?utm_source=share_via&utm_content=profile&utm_medium=member_android/" target="_blank" rel="noopener noreferrer" className="social-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
               </a>
             </div>
